f9eec960b6267fb479ce0e1967ab20b745b445f4
Pihaar <patrick@haar-family.de>
2017-03-14 16:00:56 +0100

    Creation of Folder Structure

---
+++ Ibsys2/Ibsys2/Static/Static.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ibsys2.Static {
+    static class Static {
+
+    }
+}
