c097773930a563a4f8f90e5f8667583688f8d7fd
Exitare <business.exitare@icloud.com>
2017-03-21 11:25:24 +0100

    Add new Unit Tests

--- Ibsys2/IbSysUnitTests/IbSysTests.cs
+++ Ibsys2/IbSysUnitTests/IbSysTests.cs
@@ -4,6 +4,7 @@ using Ibsys2.Static;
 using Ibsys2.Static.Input;
 using Ibsys2.Pages;
 using Ibsys2.Pages.ReadXML;
+using Ibsys2.Pages.CreateXML;
 
 namespace IbSysUnitTests
 {
@@ -13,8 +14,80 @@ namespace IbSysUnitTests
         [TestMethod]
         public void TestXMLCompile()
         {
-            Ibsys2.Pages.ReadXML.ReadXML readXML = new ReadXML();
+            ReadXML readXML = new ReadXML();
             readXML.KlickAufDateiUpload();
         }
+
+        public void TestCreateXML()
+        {
+            CreateXML createXML = new CreateXML();
+           
+        }
+
+
+        [TestMethod]
+        public void sellWishSetAnzahl()
+        {
+
+            Sellwish.setAnzahlArtikel(1 ,2);
+            Sellwish.setAnzahlArtikel(2, 2);
+            Sellwish.setAnzahlArtikel(3, 2);
+
+        }
+
+
+        [TestMethod]      
+        public void getQuantityForItem()
+        {
+            Ibsys2.Static.Input.Productionlist.getQuantityForArticle(1);
+            Ibsys2.Static.Input.Productionlist.getQuantityForArticle(2);
+            Ibsys2.Static.Input.Productionlist.getQuantityForArticle(3);
+        }
+
+        [TestMethod]
+        public void orderlistAddItem()
+        {
+            OrderlistItem item1 = new OrderlistItem(1, 2, 1);
+            OrderlistItem item2 = new OrderlistItem(2, 2, 1);
+            OrderlistItem item3 = new OrderlistItem(3, 2, 1);
+
+            Orderlist.AddItem(item1);
+            Orderlist.AddItem(item2);
+            Orderlist.AddItem(item3);
+
+
+        }
+
+
+        [TestMethod]
+        public void getOrderForItemOrderList()
+        {
+            Orderlist.getOrderForArticle(1);
+            Orderlist.getOrderForArticle(2);
+            Orderlist.getOrderForArticle(3);
+        }
+
+
+       
+         [TestMethod]
+        public void workTimeListAddItem()
+        {
+            WorkingtimelistItem item1 = new WorkingtimelistItem(1, 2, 3);
+            WorkingtimelistItem item2 = new WorkingtimelistItem(2, 2, 3);
+            WorkingtimelistItem item3 = new WorkingtimelistItem(10,1, 3);
+            Workingtimelist.AddItem(item1);
+            Workingtimelist.AddItem(item2);
+            Workingtimelist.AddItem(item3);
+        }
+
+
+        [TestMethod]
+        public void sellDirectsetAnzahlArtikel()
+        {
+            Selldirect.setAnzahlArtikel(1, 2, 3, 4);
+            Selldirect.setAnzahlArtikel(2, 2, 3, 4);
+            Selldirect.setAnzahlArtikel(3, 2, 3, 4);
+        }
+
     }
 }
