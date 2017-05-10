e26cbecb164ff08761bdb5d34f2cd19e78baa135
bolanren <piotr.pyda@icloud.com>
2017-05-10 15:34:34 +0200

    arbeitsplatz und co für kapabedarf

---
+++ Ibsys2/Ibsys2/Berechnungen/Logic/Arbeitsplatz.cs
@@ -0,0 +1,32 @@
+﻿using Ibsys2.Static.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ibsys2.Berechnungen.Logic
+{
+    public sealed class ValueStore
+    {
+        private static readonly ValueStore instance = new ValueStore();
+
+        private ValueStore() { }
+
+        public static ValueStore Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public int vertriebswunschP1;
+        public int vertriebswunschP2;
+        public int vertriebswunschP3;
+
+        public int sicherheitsbestandP1;
+        public int sicherheitsbestandP2;
+        public int sicherheitsbestandP3;
+    }
+}
