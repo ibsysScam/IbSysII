7c3e37a63be22be79eff7edf67665924c777d99c
Patrick <Patrick@MIRACOULIX>
2017-05-30 15:07:19 +0200

    Improvement for Forcast Sum

--- Ibsys2/Ibsys2/MainWindow.xaml.cs
+++ Ibsys2/Ibsys2/MainWindow.xaml.cs
@@ -177,10 +177,49 @@ namespace Ibsys2 {
 
         private void UpdateSummeFromForcast(object sender, TextChangedEventArgs e) {
             try {
-                period0sum.Text = (Convert.ToInt32(period0product1.Text) + Convert.ToInt32(period0product2.Text) + Convert.ToInt32(period0product3.Text)).ToString();
-                period1sum.Text = (Convert.ToInt32(period1product1.Text) + Convert.ToInt32(period1product2.Text) + Convert.ToInt32(period1product3.Text)).ToString();
-                period2sum.Text = (Convert.ToInt32(period2product1.Text) + Convert.ToInt32(period2product2.Text) + Convert.ToInt32(period2product3.Text)).ToString();
-                period3sum.Text = (Convert.ToInt32(period3product1.Text) + Convert.ToInt32(period3product2.Text) + Convert.ToInt32(period3product3.Text)).ToString();
+                int p0sum = 0;
+                int p1sum = 0;
+                int p2sum = 0;
+                int p3sum = 0;
+                if (period0product1 != null)
+                    p0sum += Convert.ToInt32(period0product1.Text);
+                if (period0product2 != null)
+                    p0sum += Convert.ToInt32(period0product2.Text);
+                if (period0product3 != null)
+                    p0sum += Convert.ToInt32(period0product3.Text);
+
+                if (period1product1 != null)
+                    p1sum += Convert.ToInt32(period1product1.Text);
+                if (period1product2 != null)
+                    p1sum += Convert.ToInt32(period1product2.Text);
+                if (period1product3 != null)
+                    p1sum += Convert.ToInt32(period1product3.Text);
+
+                if (period2product1 != null)
+                    p2sum += Convert.ToInt32(period2product1.Text);
+                if (period2product2 != null)
+                    p2sum += Convert.ToInt32(period2product2.Text);
+                if (period2product3 != null)
+                    p2sum += Convert.ToInt32(period2product3.Text);
+
+                if (period3product1 != null)
+                    p3sum += Convert.ToInt32(period3product1.Text);
+                if (period3product2 != null)
+                    p3sum += Convert.ToInt32(period3product2.Text);
+                if (period3product3 != null)
+                    p3sum += Convert.ToInt32(period3product3.Text);
+
+                if (period0sum != null)
+                    period0sum.Text = p0sum.ToString();
+
+                if (period1sum != null)
+                    period1sum.Text = p1sum.ToString();
+
+                if (period2sum != null)
+                    period2sum.Text = p2sum.ToString();
+
+                if (period3sum != null)
+                    period3sum.Text = p3sum.ToString();
             } catch { }
         }
 
